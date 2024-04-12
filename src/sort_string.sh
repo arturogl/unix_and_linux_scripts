84adc0eedf48bee150a956a945ae262c530eecff
Arturo Gutierrez Loza <arturogl@mac.com>
2024-04-12 16:30:24 -0600

    Create sort_string.sh

---
+++ src/sort_string.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Script Name: sort_string.sh
+# Description: Sorts a string alphabetically.
+# Usage: sort_string.sh string
+#       string - a string to be sorted
+# Example: sort_string.sh "Ala ma kota"
+# Output: Aaaaklmot
+
+validate_arguments() {
+    if [ $# -ne 1 ]; then
+        echo "Usage: sort_string.sh string"
+        echo "       string - a string to be sorted"
+        echo "Example: sort_string.sh \"Ala ma kota\""
+        exit 1
+    fi
+}
+
+sort_string() {
+    local sorted_string
+    sorted_string=$(echo "$1" | grep -o . | sort -V | tr -d "\n")
+    echo "$sorted_string"
+}
+
+main() {
+    validate_arguments "$@"
+
+    sort_string "$1"
+}
+
+main "$@"
